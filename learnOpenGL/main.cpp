91ee7f94bfe6f97928dc27a7e7d4a4e004e6edc6
nicefish <xinli2050009@gmail.com>
2020-06-18 19:56:12 +0800

    面剔除

--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -66,55 +66,112 @@ int main()
     glDepthFunc(GL_LESS);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
 
     Shader grassShader("blend.vs", "blend.fs");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+    /*
+     Remember: to specify vertices in a counter-clockwise winding order you need to visualize the triangle
+     as if you're in front of the triangle and from that point of view, is where you set their order.
+
+     To define the order of a triangle on the right side of the cube for example, you'd imagine yourself looking
+     straight at the right side of the cube, and then visualize the triangle and make sure their order is specified
+     in a counter-clockwise order. This takes some practice, but try visualizing this yourself and see that this
+     is correct.
+ */
+
+    
+    //float cubeVertices[] = {
+    //    // Back face
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+    //     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+    //     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right         
+    //     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left
+    //    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+    //    // Front face
+    //    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+    //     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+    //     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+    //     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+    //    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
+    //    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+    //    // Left face
+    //    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+    //    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+    //    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+    //    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+    //    // Right face
+    //     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+    //     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+    //     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right         
+    //     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+    //     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+    //     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left     
+    //    // Bottom face
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+    //     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left
+    //     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+    //     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+    //    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+    //    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+    //    // Top face
+    //    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+    //     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+    //     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right     
+    //     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+    //    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+    //    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left        
+    //};
+
     float cubeVertices[] = {
-        // positions          // texture Coords
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        // Back face
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right    
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right              
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left                
+        // Front face
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right        
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left        
+        // Left face
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left       
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+        // Right face
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right      
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right          
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+        // Bottom face          
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left        
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+        // Top face
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right                 
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // bottom-left  
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f  // top-left       
     };
+
     float planeVertices[] = {
         // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
          5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
