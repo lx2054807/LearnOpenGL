a72bc3d190df79036a8770b7aaf03106af1c27ae
nicefish <xinli2050009@gmail.com>
2020-06-24 17:30:29 +0800

    几种后处理

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # LearnOpenGL
 
 https://learnopengl-cn.github.io/
-目前进度: 高级OPENGL - 帧缓冲 - 后期处理(渲染到纹理还有些不理解)
+目前进度: 高级OPENGL - 立方体贴图
 
-// assimp库编译总是失败, 没有运行 模型加载 - 模型部分的代码, 只加入了mesh类和model类
+assimp库编译总是失败, 没有运行 模型加载 - 模型部分的代码, 只加入了mesh类和model类
 materials:http://devernay.free.fr/cours/opengl/materials.html
 quaternion:https://krasjet.github.io/quaternion/quaternion.pdf
 bonus_gimbal_lock:https://krasjet.github.io/quaternion/bonus_gimbal_lock.pdf
--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -201,6 +201,9 @@ int main()
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
     // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
     unsigned int rbo;
