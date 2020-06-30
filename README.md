bd57630e3e5cc5e901bbf0f16729ebb48dd94a63
nicefish <xinli2050009@gmail.com>
2020-06-30 15:20:02 +0800

    箱子反射

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # LearnOpenGL
 
 https://learnopengl-cn.github.io/
-目前进度: 高级OPENGL - 立方体贴图 - 显示天空盒
+目前进度: 高级OPENGL - 立方体贴图 - 反射
 
 assimp库编译总是失败, 没有运行 模型加载 - 模型部分的代码, 只加入了mesh类和model类
 materials:http://devernay.free.fr/cours/opengl/materials.html
@@ -9,3 +9,4 @@ quaternion:https://krasjet.github.io/quaternion/quaternion.pdf
 bonus_gimbal_lock:https://krasjet.github.io/quaternion/bonus_gimbal_lock.pdf
 素材:https://www.element3ds.com/index.php#positioning
 场景素材:http://www.custommapmakers.org/
+正则:https://regexr.com/
--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -72,54 +72,54 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    Shader shader("framebuffer.vs", "framebuffer.fs");
+    Shader shader("reflect_box.vs", "reflect_box.fs");
     Shader skyboxShader("skybox.vs", "skybox.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float cubeVertices[] = {
-        // positions          // texture Coords
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        // positions          // normals
+         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+          0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+          0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+         -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+         -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+         -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+         -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+          0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+          0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+          0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+          0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+          0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+          0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+          0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+          0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+          0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+          0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+          0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+          0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
 
     float skyboxVertices[] = {
@@ -179,10 +179,10 @@ int main()
     glBindVertexArray(cubeVAO);
     glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     // skybox VAO
@@ -196,12 +196,12 @@ int main()
     glEnableVertexAttribArray(0);
     // load textures
     // -------------
-    unsigned int cubeTexture = loadTexture("container.jpg");
     unsigned int skyboxTextures = loadCubemap(faces);
     // shader configuration
     // --------------------
+
     shader.use();
-    shader.setInt("texture1", 0);
+    shader.setInt("skybox", 0);
 
     skyboxShader.use();
     skyboxShader.setInt("skybox", 0);
@@ -239,11 +239,12 @@ int main()
         shader.setMat4("view", view);
         shader.setMat4("projection", projection);
         shader.setMat4("model", model);
+        shader.setVec3("cameraPos", camera.Position);
 
         // cubes
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, cubeTexture);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTextures);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
 
